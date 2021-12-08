c507eb6cedf28563020126b99b4e291745e71086
Desiree Mendes <desireefrost@live.ca>
2021-12-08 14:26:03 -0500

    Add interviewer story and refactor

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,14 +2,13 @@ import React from "react";
 import "components/InterviewerListItem";
 import classNames from "classnames";
 
-export default function InterviewerListItem (props) {
-
-  const InterviewerListItemClass = classNames(
-    "interviewers__item", 
-    {"interviewers__item--selected": props.selected});
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  })
 
   return (
-    <li className={InterviewerListItemClass} onClick={props.onChange}>
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
