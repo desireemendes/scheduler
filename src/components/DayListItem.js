ca1c198c0982be8a781e359fd548190dc7bdf3a7
Desiree Mendes <desireefrost@live.ca>
2021-12-08 09:49:46 -0500

    Create InterviewerListItem and story for it

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const dayListItemClass = classNames(
+    "day-list__item",
+    {'day-list__item--selected': props.selected},
+    {'day-list__item--full': props.spots === 0},
+  )
+  // const handleClick = () => props.onChange(props.name)
+  const formatSpots = (spotsRemaining) => {
+    if (!spotsRemaining) {
+      return `no spots remaining`;
+    }
+    if (spotsRemaining === 1) {
+      return `1 spot remaining`;
+    }
+    return `${spotsRemaining} spots remaining`;
+  }
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayListItemClass} >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
