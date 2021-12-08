a28864575f1d5cf644d7d67819e588fd99a63bb5
Desiree Mendes <desireefrost@live.ca>
2021-12-08 16:46:33 -0500

    Create empty function and return html

---
+++ src/components/Appointment/empty.js
@@ -0,0 +1,11 @@
+export default function empty() {
+  return (
+    <main className="appointment__add">
+  <img
+    className="appointment__add-button"
+    src="images/add.png"
+    alt="Add"
+  />
+</main>
+  )
+}
