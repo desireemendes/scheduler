4af24ee9ed71231a8beec758469256116ce6be27
Desiree Mendes <desireefrost@live.ca>
2021-12-08 12:49:32 -0500

    Build InterviewerListItem with props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,27 @@
-import "components/InterviewerListItem.scss";
+import React from "react";
+import "components/InterviewerListItem";
+import classNames from "classnames";
 
-export default function setInterviewer () {
+export default function InterviewerListItem (props) {
+
+  const InterviewerListItemClass = classNames(
+    "interviewers__item", 
+    {"interviewers__item--selected": props.selected});
+
   return (
-    <li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li>
-  )
+    <li className={InterviewerListItemClass} onClick={props.onChange}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
 }
+
+//1. replace hardcoded values with props
+// interviewer props = id, name, avatar
+//2. add event listener to the li, each item should be clickable and setInterviewer function should run,
+//taking interview id as a parameter
+//3. interviewers__item--selected should only be applied if interviewer is selected
