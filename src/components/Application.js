52f12df674b1c80aaf9926d150fba56aef4b0061
Desiree Mendes <desireefrost@live.ca>
2021-12-10 22:43:14 -0500

    getInterviewersForDay function and tests, add interviews selector

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 import axios from "axios";
 import { getAppointmentsForDay } from "helpers/selectors";
 import { getInterview } from "helpers/selectors";
+import { getInterviewersForDay } from "helpers/selectors";
 
 // const appointments = [
 //   {
@@ -67,79 +68,75 @@ import { getInterview } from "helpers/selectors";
 // ];
 
 export default function Application(props) {
-  const setDay = day => setState({ ...state, day });
+  const setDay = (day) => setState({ ...state, day });
   // const setDays = (days) => setState(prev => ({ ...prev, days }));
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {
-    },
-    interviewers: {}
+    appointments: {},
+    interviewers: {},
   });
   // const dailyAppointments = getAppointmentsForDay(state, state.day)
   const appointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
 
-const schedule = appointments.map((appointment) => {
-  const interview = getInterview(state, appointment.interview);
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
 
-  return (
-    <Appointment
-      key={appointment.id}
-      id={appointment.id}
-      time={appointment.time}
-      interview={interview}
-    />
-  );
-});
+    return (
+      <Appointment
+        // key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+        interviewers={interviewers}
+      />
+    );
+  });
   useEffect(() => {
     // axios.get('http://localhost:8001/api/days')
     // .then((response) => {
     //   console.log(response.data)
     // })
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers"),
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-      console.log(all[2].data) 
-    
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+      console.log(all[2].data);
+
       const [first, second, third] = all;
-    
       console.log(first, second, third);
-    });
-}, [])
+    })
+    .catch(err => console.log(err))
+  }, []);
 
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList 
-  days={state.days} 
-  value={state.day} 
-  onChange={setDay} 
-/>
-
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={state.days} value={state.day} onChange={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-      {/* {dailyAppointments.map((appointment) => 
-          <Appointment
-            key={appointment.id}
-            {...appointment}
-          />
-        )} */}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,23 +4,31 @@ import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
 import useVisualMode from "hooks/useVisualMode";
-
+import Form from "./Form";
 
 export default function Appointment(props) {
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = 'CREATE';
 
 const { mode, transition, back } = useVisualMode(
   props.interview ? SHOW : EMPTY
 );
 
+const onAdd = () => transition(CREATE);
+const onCancel = () => back();
+
+
   return (
     <article className="appointment">
-    <Header time={props.time} />
+    {/* <Header time={props.time} />
     {props.interview ? 
     <Show 
     student={props.interview.student}
-    interviewer={props.interview.interviewer} /> : <Empty />}
+    interviewer={props.interview.interviewer} /> : <Empty />} */}
+        {mode === 'EMPTY' && <Empty onAdd={onAdd} />}
+      {mode === 'SHOW' && <Show interview={props.interview} /> }
+      {mode === 'CREATE' && <Form onCancel={onCancel}/>}
   </article>
   )
 }
