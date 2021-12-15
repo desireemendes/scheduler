525ffaf385eefecb3300ab95f4868addbca407c0
Desiree Mendes <desireefrost@live.ca>
2021-12-14 19:44:23 -0500

    Test for rendering appointment

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
+
 export default function InterviewerListItem(props) {
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected
