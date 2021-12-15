803c41b56247402589e11f9cc6feba6ccc1bac0b
Desiree Mendes <desireefrost@live.ca>
2021-12-15 12:22:34 -0500

    Fix dayList to be able to click on days

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -20,7 +20,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li className={dayListItemClass} >
+    <li className={dayListItemClass} onClick={() => props.setDay(props.name)} selected={props.selected} >
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
